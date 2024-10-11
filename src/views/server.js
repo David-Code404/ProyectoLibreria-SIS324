37fd523991733fb5b479d926a579171f33d21bfe
David Code 404 <josesucullani@gmail.com>
2024-10-11 14:00:04 +0000

    actualizando server

--- src/views/server.js
+++ src/views/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
 const device = require('express-device');
-const apiRouter = require('../routes/api');
+const apiRouter = require('../routes/api'); // Declarado una sola vez
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Definir el puerto del servidor
 
-
 // Middleware para detectar el dispositivo del usuario
 app.use(device.capture());
-
 
 // <!---------------------Middleware para registrar accesos a la página-------------------------->
 const uniqueIPs = new Set();
@@ -26,25 +24,18 @@ app.use((req, res, next) => {
 // <!--------Menejar solicitudes JSON------------->
 app.use(express.json());
 
-
-
 // <!-----------------middleware para servir archivos estáticos de 'public'---------------------->
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/api', apiRouter);
-
-
+app.use('/api', apiRouter); // Usar apiRouter aquí
 
 // Ruta para dispositivos móviles
 app.get('/', (req, res) => {
     if (req.device.type === 'phone' || req.device.type === 'tablet') {
         res.sendFile(path.join(__dirname, '..', '..', 'public', 'pages', 'mobile.html'));
     } else {
-        res.sendFile(path.join(__dirname, '..', '..', 'public', 'pages', 'index.html'));
+        res.sendFile(path.join(__dirname, '..', '..', 'public', 'pages', 'home.html'));
     }
 });
-
-
-
 
 // <!---------middleware para manejar errores 404 y errores internos del servidor---------------->
 app.use((req, res) => {
@@ -55,8 +46,6 @@ app.use((err, req, res) => {
     res.status(500).send('Error interno del servidor');
 });
 
-
-
 // <!-------------------Iniciar servidor y escuchar puertos especificados------------------------>
 app.listen(PORT, () => {
     // códigos ANSI para colores y estilos en la terminal:
@@ -66,7 +55,7 @@ app.listen(PORT, () => {
     console.log('\x1b[32m%s\x1b[0m', `Para acceder a la aplicación, visita: http://localhost:${PORT}`);
 });
 
-
+// Ruta para obtener categorías
 app.get("/api/categorias", (req, res) => {
     const nombre = req.query.nombre;
     const sql = nombre ? 
@@ -95,11 +84,3 @@ app.post("/api/categorias", (req, res) => {
         res.status(201).json({ id: this.lastID, nombre });
     });
 });
-
-
-
-// Asegúrate de que apiRouter esté incluido
-const apiRouter = require('../routes/api');
-
-// Middleware para manejar las rutas de la API
-app.use('/api', apiRouter);
